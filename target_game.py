e4481d229cccd93e0e5fcee164b86647ec3a2bfb
kostyantin <kostyantin1408@gmail.com>
2022-10-28 23:40:21 +0300

    finally finished the game

--- target_game.py
+++ target_game.py
@@ -1,7 +1,14 @@
-from math import isclose
+"""
+This module represents the game 'Target'
+"""
 from typing import List
 import random
 import string
+
+LIST_OF_LETTERS = []
+WORDS_FROM_THE_DICTIONARY = []
+USER_WORDS = []
+INCORRECT_WORDS = []
 
 
 def generate_grid():
@@ -16,14 +23,18 @@ def generate_grid():
         while len(current) != 3:
             current.append(random.choice(alphabet))
         grid.append(current)
+    global LIST_OF_LETTERS
+    LIST_OF_LETTERS = grid
     return grid
 
 
 def get_words(f: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
-    >>> get_words('/Users/kostyantin/UCUpython/target_game/en.txt', ['w', 'u', 'm', 'r', 'o', 'v', 'k', 'i', 'f'])
-    ['fork', 'form', 'forum', 'four', 'fowk', 'from', 'frow', 'irok', 'komi', 'kori', 'miro', 'moki', 'ovum', 'work', 'worm', 'wouf']
+    >>> get_words('/Users/kostyantin/UCUpython/target_game/en.txt',\
+     ['w', 'u', 'm', 'r', 'o', 'v', 'k', 'i', 'f'])
+    ['fork', 'form', 'forum', 'four', 'fowk', 'from', 'frow',\
+'irok', 'komi', 'kori', 'miro', 'moki', 'ovum', 'work', 'worm', 'wouf']
     """
     central_letter = letters[4]
     list_of_all_words = []
@@ -39,6 +50,8 @@ def get_words(f: str, letters: List[str]) -> List[str]:
             else:
                 if word not in list_of_suited_words:
                     list_of_suited_words.append(word)
+    global WORDS_FROM_THE_DICTIONARY
+    WORDS_FROM_THE_DICTIONARY = list_of_all_words
     return list_of_suited_words
 
 
@@ -46,7 +59,7 @@ def get_words(f: str, letters: List[str]) -> List[str]:
 def get_user_words() -> List[str]:
     """
     Gets words from user input and returns a list with these words.
-    Usage: enter a word or press ctrl+d to finish for *nix or Ctrl-Z+Enter 
+    Usage: enter a word or press ctrl+d to finish for *nix or Ctrl-Z+Enter
     for Windows.
     Note: the user presses the enter key after entering each word.
     """
@@ -59,18 +72,47 @@ def get_user_words() -> List[str]:
             users_list_of_words.append(word)
         else:
             continue
+    global USER_WORDS
+    USER_WORDS = users_list_of_words
     return users_list_of_words
 
 
-def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+def get_pure_user_words(user_words: List[str], letters: List[str],\
+words_from_dict: List[str]) -> List[str]:
     """
     (list, list, list) -> list
 
     Checks user words with the rules and returns list of those words
     that are not in dictionary.
     """
-    pass
+    words_that_suited_the_rools = []
+
+    for word in user_words:
+        if len(word) < 4 or letters[4] not in word:
+            user_words.remove(word)
+    for word in user_words:
+        if word.lower()  in words_from_dict or word.capitalize() in words_from_dict:
+            continue
+        else:
+            for j in word:
+                flag = True
+                if word.count(j) <= letters.count(j):
+                    continue
+                else:
+                    flag = False
+                    break
+            if flag:
+                words_that_suited_the_rools.append(word)
+    global INCORRECT_WORDS
+    INCORRECT_WORDS = words_that_suited_the_rools
+    return words_that_suited_the_rools
 
 
 def results():
-    pass
+    """
+    Represents the result of the game
+    """
+    return f"Given list of letters - {LIST_OF_LETTERS}\
+    Words from the dictionary, that suited the ocnditions - {WORDS_FROM_THE_DICTIONARY}\
+    Words, that user inputed - {USER_WORDS}\
+    Users fords, that don't suit the conditions - {INCORRECT_WORDS}"
